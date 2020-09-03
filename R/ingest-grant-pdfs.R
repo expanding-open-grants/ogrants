2be444d3dcbebd3864b4eb870b20991a077c4a6f
Hao Ye <lhopitalified@gmail.com>
2020-09-03 15:41:17 -0400

    update R script to generate docs correctly

--- R/ingest-grant-pdfs.R
+++ R/ingest-grant-pdfs.R
@@ -6,10 +6,16 @@ source(here::here("R", "retrieve_pdf.R"))
 ES_index_name <- "ogrants"
 
 ## get csv from website
-csv_path <- file.path(tempdir(), "opengrants.csv")
-download.file("https://expanding-open-grants.github.io/ogrants/opengrants.csv",
-              csv_path)
+csv_path <- here::here("_site", "opengrants.csv")
+if (!file.exists(csv_path))
+{
+  download.file("https://expanding-open-grants.github.io/ogrants/opengrants.csv",
+                csv_path)
+}
 grants_df <- readr::read_csv(csv_path)
+attr(grants_df, "problems") <- NULL
+attr(grants_df, "spec") <- NULL
+attr(grants_df, "class") <- "data.frame"
 
 ES <- connect()
 
@@ -31,26 +37,37 @@ pipeline_create(ES, id = 'pdfin', body = boddy)
 # check if index exists
 if (!index_exists(ES, ES_index_name)) index_create(ES, ES_index_name)
 
-# attach function
-make_body_attach <- function(binary_dat)
-{
-  paste0('{\n  "fulltext": "', base64enc::base64encode(binary_dat), '"\n}')
-}
-
 ## loop over grants data
 for (i in seq_len(NROW(grants_df)))
 {
-  # TODO: check if `id` = grants_df$id[i] exists as a document
+  baseurl <- "https://expanding-open-grants.github.io/ogrants/"
+  
+  # construct body from grants_df
+  body <- as.list(grants_df[i,])
+  body$url <- paste0(baseurl, body$url)
+  body$url <- gsub("(?<!https:)//", "/", body$url, perl = TRUE)
+  body$url <- gsub("(ogrants/){2,}", "ogrants/", body$url)
+  body$link <- extract_raw_link(body$link)
   
   # retrieve pdf (probably should be wrapped to timeout)
   pdf_binary_dat <- get_binary_pdf_from_link(grants_df$link[i])
-  if (is.null(pdf_binary_dat)) { next() }
-  
-  # attach pdf
-  pipeline_attachment(ES,
-                      index = ES_index_name, 
-                      id = grants_df$id[i], 
-                      pipeline = "pdfin", 
-                      make_body_attach(pdf_binary_dat))
+  if (!is.null(pdf_binary_dat))
+  {
+    # create temporary doc with temporary id for attachment ingestion
+    stash_id <- "temp"
+    pipeline_attachment(ES,
+                        index = ES_index_name, 
+                        id = stash_id, 
+                        pipeline = "pdfin", 
+                        make_body_attach(pdf_binary_dat))
+    pdf_content <- docs_get(ES, ES_index_name, stash_id)
+    body$attachment <- pdf_content$`_source`$attachment
+  }
+
+  # add document
+  docs_create(ES, ES_index_name, 
+              id = body$id, 
+              body = body)
 }
 
+docs_delete(ES, ES_index_name, stash_id)
